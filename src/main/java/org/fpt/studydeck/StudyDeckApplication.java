395cfaf7e9465ad84e88203cd024e5e578ebe763
tientvhe181590-beep <tientvhe181590@fpt.edu.vn>
2026-06-10 15:48:33 +0700

    chore: commit spring boot scaffold

---
+++ src/main/java/org/fpt/studydeck/StudyDeckApplication.java
@@ -0,0 +1,13 @@
+package org.fpt.studydeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDeckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyDeckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fpt/studydeck/StudyDeckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fpt.studydeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDeckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
